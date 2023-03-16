5293ae8c221092376bc1a6d696952723cd02c422
sasi_007 <rahd006@gmail.com>
2023-03-16 21:29:15 +0530

    New initial commit

---
+++ src/playground/redux-101.js
@@ -0,0 +1,71 @@
+import { createStore } from 'redux';
+
+const incrementCount = ({ incrementBy = 1 } = {}) => ({
+    type: 'INCREMENT',
+    incrementBy
+});
+
+const decrementCount = ({ decrementBy = 1 } = {}) => ({
+    type: 'DECREMENT',
+    decrementBy
+});
+
+const setCount = ({ count } = {}) => ({
+    type: 'SET',
+    count
+});
+
+const resetCount = ({ } = {}) => ({
+    type: 'RESET'
+});
+
+//Reducers
+// 1. Reducers are pure functions // it means the output value fully depends on input value it should not depend on global variable
+// 2. Never change state or action // it means the state or action should never change by user instead we have give the value to new variable
+
+const countReducer = (state = { count: 0 }, action) => {
+    switch (action.type) {
+        case 'INCREMENT':
+            return {
+                count: state.count + action.incrementBy
+            };
+        case 'DECREMENT':
+            return {
+                count: state.count - action.decrementBy
+            };
+        case 'RESET':
+            return {
+                count: 0
+            };
+        case 'SET':
+            return {
+                count: action.count
+            }
+        default:
+            return state;
+    }
+};
+
+const store = createStore(countReducer);
+
+const unsubscribe = store.subscribe(() => {
+    console.log(store.getState());
+});
+
+store.dispatch(incrementCount({ incrementBy: 5 }));
+
+store.dispatch(incrementCount());
+
+// unsubscribe();
+
+store.dispatch(resetCount());
+
+store.dispatch(decrementCount());
+
+store.dispatch(decrementCount({ decrementBy: 10 }));
+
+store.dispatch(setCount({ count: 101 }));
+
+store.dispatch(resetCount());
+
+store.dispatch(setCount({ count: 102 }));
